733627dc739a56112166b04229e6db4e6edfcb98
Zhasik0312 <126457650+Zhasik0312@users.noreply.github.com>
2024-02-26 08:48:22 +0600

    Add files via upload

---
+++ lab44.py
@@ -0,0 +1,20 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    print(node)
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            print(f"bardy->{neighbor}")
+            dfs(graph, neighbor, visited)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': [],
+}
+
+visited = set()
+dfs(graph, 'A', visited)
